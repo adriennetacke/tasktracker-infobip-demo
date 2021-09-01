7eea10d2e303e4a603b40fa07dfeacfc0cf1803d
Adrienne Tacke <adrienne.tacke@gmail.com>
2021-09-01 00:33:17 -0700

    Updates

---
+++ precache-manifest.ef0fffab9d47ba2752c7958d3a1a379f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93af2d3ae6ac8bb32d3405a28d2835aa",
+    "url": "/tasktracker-infobip-demo/index.html"
+  },
+  {
+    "revision": "51338d590c8dafe2677c",
+    "url": "/tasktracker-infobip-demo/static/css/main.889bdc5e.chunk.css"
+  },
+  {
+    "revision": "6c53e385187a1476848f",
+    "url": "/tasktracker-infobip-demo/static/js/2.7ffc3e6e.chunk.js"
+  },
+  {
+    "revision": "697503643580ca28ddbc8f0a1738cdb1",
+    "url": "/tasktracker-infobip-demo/static/js/2.7ffc3e6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51338d590c8dafe2677c",
+    "url": "/tasktracker-infobip-demo/static/js/main.aa2ff84f.chunk.js"
+  },
+  {
+    "revision": "f52a19d84244ba98ebfa",
+    "url": "/tasktracker-infobip-demo/static/js/runtime-main.23b35d57.js"
+  }
+]);
